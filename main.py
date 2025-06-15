7ad62baa6187a1e6899a6f56270841430d951f1e
voltrecto <voltairerecto@gmail.com>
2025-06-15 16:29:41 -0500

    Add verbose tag

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 import sys
 
 def main():
@@ -9,17 +10,23 @@ def main():
         print("Please provide command line argument for the prompt.")
         sys.exit(1)
 
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=args[1])]),
+    ]
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents=' '.join(sys.argv[1:])
+        model='gemini-2.0-flash-001', contents=messages
     )
 
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if "--verbose" in args:
+        print(f"User prompt: {args[1]}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
