2fd1b168388601989d82ea52b45db282306c17c6
voltrecto <voltairerecto@gmail.com>
2025-06-15 16:05:38 -0500

    Add command line argument

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
+def main():
+    args = sys.argv
+    if len(args) < 2:
+        print("Please provide command line argument for the prompt.")
+        sys.exit(1)
 
-response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
-)
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
 
-print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', contents=' '.join(sys.argv[1:])
+    )
+
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+if __name__ == "__main__":
+    main()
