ed008b5f4a0340761b463f03652ac7a6080581c6
voltrecto <voltairerecto@gmail.com>
2025-07-20 23:05:49 -0500

    Add system_prommpt variable

--- main.py
+++ main.py
@@ -10,16 +10,23 @@ def main():
         print("Please provide command line argument for the prompt.")
         sys.exit(1)
 
+    
+    model_name = 'gemini-2.0-flash-001'
+
     messages = [
         types.Content(role="user", parts=[types.Part(text=args[1])]),
     ]
+
+    system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents=messages
+    model=model_name,
+    contents=messages,
+    config=types.GenerateContentConfig(system_instruction=system_prompt),
     )
 
     print(response.text)
