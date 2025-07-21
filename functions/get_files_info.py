075006ec4c38ec8b1d1377dc66ab06f2aa9e0f84
voltrecto <voltairerecto@gmail.com>
2025-07-20 23:35:06 -0500

    Add function call checks

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,4 +1,5 @@
 import os
+from google.genai import types
 
 def get_files_info(working_directory, directory=None):
     try:
@@ -21,3 +22,17 @@ def get_files_info(working_directory, directory=None):
         return '\n'.join(content_list)
     except Exception as e:
         return f"Error: {e}"
+
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 import sys
+from functions.get_files_info import schema_get_files_info
 
 def main():
     args = sys.argv
@@ -12,12 +13,23 @@ def main():
 
     
     model_name = 'gemini-2.0-flash-001'
-
     messages = [
         types.Content(role="user", parts=[types.Part(text=args[1])]),
     ]
+    system_prompt = """
+    You are a helpful AI coding agent.
 
-    system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
+    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+    - List files and directories
+
+    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """    
+    available_functions = types.Tool(
+    function_declarations=[
+            schema_get_files_info,
+        ]
+    )
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -26,10 +38,16 @@ def main():
     response = client.models.generate_content(
     model=model_name,
     contents=messages,
-    config=types.GenerateContentConfig(system_instruction=system_prompt),
+    config=types.GenerateContentConfig(
+        tools=[available_functions], system_instruction=system_prompt
+        ),
     )
 
-    print(response.text)
+    if response.function_calls:
+        for function_call_part in response.function_calls:
+            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        print(response.text)
     if "--verbose" in args:
         print(f"User prompt: {args[1]}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
